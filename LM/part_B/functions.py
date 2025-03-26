043c102842527bf920d0067ba1cd931844427049
marcomorandin <emmeemmenove@gmail.com>
2025-03-26 12:06:35 +0100

    .

--- LM/part_B/functions.py
+++ LM/part_B/functions.py
@@ -1,2 +1,111 @@
-# Add the class of your model only
-# Here is where you define the architecture of your model using pytorch
+import torch.nn as nn
+import torch
+import math
+import matplotlib.pyplot as plt
+import os
+import json
+
+def train_loop(data, optimizer, criterion, model, clip=5):
+    model.train()
+    loss_array = []
+    number_of_tokens = []
+    
+    for sample in data:
+        optimizer.zero_grad()
+        output = model(sample['source'])
+        loss = criterion(output, sample['target'])
+        loss_array.append(loss.item() * sample["number_tokens"])
+        number_of_tokens.append(sample["number_tokens"])
+        loss.backward()
+        torch.nn.utils.clip_grad_norm_(model.parameters(), clip)  
+        optimizer.step()
+        
+    return sum(loss_array)/sum(number_of_tokens)
+
+def eval_loop(data, eval_criterion, model):
+    model.eval()
+    loss_to_return = []
+    loss_array = []
+    number_of_tokens = []
+    with torch.no_grad():
+        for sample in data:
+            output = model(sample['source'])
+            loss = eval_criterion(output, sample['target'])
+            loss_array.append(loss.item())
+            number_of_tokens.append(sample["number_tokens"])
+            
+    ppl = math.exp(sum(loss_array) / sum(number_of_tokens))
+    loss_to_return = sum(loss_array) / sum(number_of_tokens)
+    return ppl, loss_to_return
+
+def init_weights(mat):
+    for m in mat.modules():
+        if type(m) in [nn.GRU, nn.LSTM, nn.RNN]:
+            for name, param in m.named_parameters():
+                if 'weight_ih' in name:
+                    for idx in range(4):
+                        mul = param.shape[0]//4
+                        torch.nn.init.xavier_uniform_(param[idx*mul:(idx+1)*mul])
+                elif 'weight_hh' in name:
+                    for idx in range(4):
+                        mul = param.shape[0]//4
+                        torch.nn.init.orthogonal_(param[idx*mul:(idx+1)*mul])
+                elif 'bias' in name:
+                    param.data.fill_(0)
+        else:
+            if type(m) in [nn.Linear]:
+                torch.nn.init.uniform_(m.weight, -0.01, 0.01)
+                if m.bias != None:
+                    m.bias.data.fill_(0.01)     
+                    
+
+def generate_plot(epochs, data, labels, title, xlabel, ylabel, filename):
+    plt.figure(figsize=(10, 6))
+    for d, label in zip(data, labels):
+        plt.plot(epochs, d, label=label, marker='o')
+    plt.title(title)
+    plt.xlabel(xlabel)
+    plt.ylabel(ylabel)
+    plt.legend()
+    plt.grid(True)
+    plt.tight_layout()
+    plt.savefig(filename)
+    plt.close()
+
+def report(best_model, epochs, loss_train, loss_dev, perplexity_list, final_ppl, point, Config, path):
+    generate_plot(
+        epochs=epochs,
+        data=[loss_train, loss_dev],
+        labels=['Training Loss', 'Validation Loss'],
+        title='Training and Validation Loss',
+        xlabel='Epochs',
+        ylabel='Loss',
+        filename=os.path.join(path, 'loss_plot.png')
+    )
+
+    generate_plot(
+        epochs=epochs,
+        data=[perplexity_list],
+        labels=['Validation Perplexity'],
+        title='Validation Perplexity',
+        xlabel='Epochs',
+        ylabel='Perplexity',
+        filename=os.path.join(path, 'ppl_plot.png')
+    )
+    
+    report_data = {
+        "number_epochs": len(epochs),
+        "lr": Config.lr,
+        "hidden_size": Config.hid_size,
+        "emb_size": Config.emb_size,
+        "clip": Config.clip,
+        "batch_size": Config.batch_size,
+        "eval_batch_size": Config.eval_batch_size,
+        "point": point,
+        "final_ppl": final_ppl
+    }
+    
+    with open(os.path.join(path, 'report.json'), "w") as file:
+        json.dump(report_data, file, indent=4)
+    
+    torch.save(best_model.state_dict(), os.path.join(path, "model.pt"))
--- LM/part_B/main.py
+++ LM/part_B/main.py
@@ -1,9 +1,160 @@
-# This file is used to run your functions and print the results
-# Please write your fuctions or classes in the functions.py
-
-# Import everything from functions.py file
 from functions import *
+from utils import *
+from model import *
+
+from functools import partial
+from torch.utils.data import DataLoader
+import torch.optim as optim
+import os
+import matplotlib.pyplot as plt
+from tqdm import tqdm
+import copy
+import math
+import numpy as np
+from dataclasses import dataclass
+from itertools import product
+import json
+
+@dataclass
+class Config:
+    hid_size: int = 400
+    emb_size: int = 400
+    lr: float = 0.0008
+    clip: float = 5
+    batch_size: int = 128
+    eval_batch_size: int = 128
+    n_epochs: int = 100
+    patience: int = 3
+
+
+def main(point = "3",  config=Config(), logger = None, report_path = './report'):
+    device = get_device(logger)
+    
+    logger.debug(f"Starting training with configuration: {config}")
+    if point == "1":
+        logger.info("Model Variant: LSTM with Weight Tying")
+    elif point == "2":
+        logger.info("Model Variant: LSTM with Weight Tying and Variational Dropout")
+    elif point == "3":
+        logger.info("Model Variant: LSTM with Weight Tying and Variational Dropout and AvSGD")
+    else:
+        logger.error("Invalid Variant!")
+        return
+    
+    cwd = os.getcwd()
+    data_paths = {
+        'train': f"{cwd}/dataset/PennTreeBank/ptb.train.txt",
+        'dev': f"{cwd}/dataset/PennTreeBank/ptb.valid.txt",
+        'test': f"{cwd}/dataset/PennTreeBank/ptb.test.txt"
+    }
+    
+    for split, path in data_paths.items():
+        if not os.path.exists(path):
+            logger.error(f"{split.capitalize()} file not found at: {path}")
+            return
+        logger.debug(f"Found {split} data at: {path}")
+
+    train_data = read_file(data_paths['train'])
+    lang = Lang(train_data, ["<pad>", "<eos>"])
+    
+    datasets = {
+        'train': PennTreeBank(train_data, lang),
+        'dev': PennTreeBank(read_file(data_paths['dev']), lang),
+        'test': PennTreeBank(read_file(data_paths['test']), lang)
+    }
+
+    loaders = {
+        'train': DataLoader(
+            datasets['train'],
+            batch_size=config.batch_size,
+            shuffle=True,
+            collate_fn=partial(collate_fn, pad_token=lang.word2id["<pad>"], device=device)
+        ),
+        'dev': DataLoader(
+            datasets['dev'],
+            batch_size=config.eval_batch_size,
+            collate_fn=partial(collate_fn, pad_token=lang.word2id["<pad>"], device=device)
+        ),
+        'test': DataLoader(
+            datasets['test'],
+            batch_size=config.eval_batch_size,
+            collate_fn=partial(collate_fn, pad_token=lang.word2id["<pad>"], device=device)
+        )
+    }
+    
+    model_class = LM_LSTM_WT if point == "1" else LM_LSTM_VD
+    
+    model = model_class(
+        config.emb_size,
+        config.hid_size,
+        len(lang.word2id),
+        pad_index=lang.word2id["<pad>"]
+    )
+    
+    model.to(device)
+    model.apply(init_weights)
+    
+    if point in ["1", "2"]:
+        optimizer = optim.SGD(model.parameters(), lr=config.lr)
+    else:
+        optimizer = AvSGD(model.parameters(), lr=config.lr)
+        
+    criterion_train = nn.CrossEntropyLoss(ignore_index=lang.word2id["<pad>"])
+    criterion_eval = nn.CrossEntropyLoss(ignore_index=lang.word2id["<pad>"], reduction='sum')
+
+    
+    losses_train = []
+    losses_dev = []
+    sampled_epochs = []
+    ppl_dev_list = []
+    best_ppl = math.inf
+    best_model = None
+    
+    for epoch in tqdm(range(config.n_epochs), desc="Training"):
+        loss = train_loop(loaders['train'], optimizer, criterion_train, model, config.clip)    
+        if epoch % 1 == 0:
+            sampled_epochs.append(epoch)
+            losses_train.append(np.asarray(loss).mean())
+            
+            ppl_dev, loss_dev = eval_loop(loaders['dev'], criterion_eval, model)
+            ppl_dev_list.append(ppl_dev)
+            
+            logger.info(f"\nEpoch {epoch}: Train Loss: {loss_dev:.4f}, Val PPL: {ppl_dev:.4f}")
+            losses_dev.append(np.asarray(loss_dev).mean())
+
+            if  ppl_dev < best_ppl:
+                best_ppl = ppl_dev
+                best_model = copy.deepcopy(model).to('cpu')
+                config.patience = 3
+            else:
+                config.patience -= 1
+                
+            if config.patience <= 0:
+                logger.info("Early stopping triggered")
+                break
+
+    best_model.to(device)
+    final_ppl,  _ = eval_loop(loaders['test'], criterion_eval, best_model)    
+    logger.info(f"Final Test PPL: {final_ppl:.4f}")
+    
+    report(best_model, sampled_epochs, losses_train, losses_dev, ppl_dev_list, final_ppl, point, config, report_path)
+
 
 if __name__ == "__main__":
-    #Wrtite the code to load the datasets and to run your functions
-    # Print the results
+    import argparse
+    logger = setup_logging()
+    parser = argparse.ArgumentParser()
+    
+    parser.add_argument('point', type=str,
+                        help=f"Point to be run. Choose from:\n 1 - LSTM, 2 - LSTM with Dropout, 3 - LSTM with Dropout and AdamW",
+                        default="3")
+    
+    parser.add_argument('path', type=str,
+                        help="Path to save the plots and report")
+    
+    args = parser.parse_args()
+    
+    if not os.path.exists(args.path):
+        os.makedirs(args.path)
+    
+    main(point=args.point, logger=logger, report_path=args.path)
